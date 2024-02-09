0545c55854cadcf8f135d759d8f0b634158637b8
benounnas <oussama@whitebay.limited>
2024-02-09 12:46:56 +0100

    init repo

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(null)
+const links = [
+  ['mdi-graph-outline', 'Graphs'],
+]
+
+</script>
+
+<template>
+  <v-app id="inspire">
+
+    <v-navigation-drawer v-model="drawer">
+      <v-sheet
+        color="grey-lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey-darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>BENOUNNAS Oussama</div>
+      </v-sheet>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          :prepend-icon="icon"
+          :title="text"
+          link
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Didata graph</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
